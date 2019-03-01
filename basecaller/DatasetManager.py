8149e00c0bd3ae1aacc5fc257fe8093fb43eff99
plubon <lubon@student.mini.pw.edu.pl>
2019-03-01 16:33:24 +0100

    working data stream

--- basecaller/DatasetManager.py
+++ basecaller/DatasetManager.py
@@ -3,7 +3,8 @@ import random
 import h5py
 import numpy as np
 import keras
-from math import ceil
+import datetime
+from math import ceil, floor
 from sklearn.model_selection import train_test_split
 
 class SignalSequence(keras.utils.Sequence):
@@ -18,9 +19,10 @@ class SignalSequence(keras.utils.Sequence):
 		self.batch_read_lens = random.choices(read_lens, k=len(self))
 		if dir_probs is None:
 			self.dir_probs = [1/len(self.file_paths.keys())]*len(self.file_paths.keys())
+			print(self.dir_probs)
 		else:
 			self.dir_probs = dir_probs
-		self.dir_counts = {key:floor(batch_size*value) for (key, value) in self.dir_probs}
+		self.dir_counts = {list(file_paths.keys())[idx]:floor(batch_size*value) for idx, value in enumerate(self.dir_probs)}
 		if sum(self.dir_counts.values()) < self.batch_size:
 			add_to_keys = random.choices(self.dir_counts.keys(), weights=self.dir_probs, k=sum(self.dir_counts.values())-self.batch_size)
 			for key in add_to_keys:
@@ -36,7 +38,7 @@ class SignalSequence(keras.utils.Sequence):
 		for i in range(len(self)):
 			batch = []
 			for key in self.file_paths.keys():
-				batch += self.dir_sequences[key][self.dir_counts*i:self.dir_counts*(i+1)]
+				batch += self.dir_sequences[key][self.dir_counts[key]*i:self.dir_counts[key]*(i+1)]
 			random.shuffle(batch)
 			self.batches.append(batch)
 
@@ -54,7 +56,7 @@ class SignalSequence(keras.utils.Sequence):
 		Y = [self.seq_to_label(y) for y in Y]
 		max_len = max([len(y) for y in Y])
 		Y_array = np.full((len(Y), max_len), -1)
-		for idx, word in enumerate(len(Y)):
+		for idx, word in enumerate(Y):
 			Y_array[idx, 0:len(word)] = word 
 		inputs = {
 			'the_input': np.stack(X),
@@ -69,20 +71,20 @@ class DataDirectoryReader:
 
 	def __init__(self, path):
 		self.base_path = path
-		self.type_dirs = os.listdir(self.path)
+		self.type_dirs = os.listdir(self.base_path)
 		self.files = dict()
 		for type_dir in self.type_dirs:
 			dir_path = os.path.join(self.base_path, type_dir)
 			self.files[type_dir] = [file for file in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path,file))]
 
 	def get_train_test_files(self):
-		train = dict()
-		test = dict()
+		train_dict = dict()
+		test_dict = dict()
 		for type_dir in self.type_dirs:
 			train, test = train_test_split(self.files[type_dir], test_size=0.2)
-			train[type_dir] = [os.path.join(self.base_path, type_dir, x) for x in train]
-			test[type_dir] = [os.path.join(self.base_path, type_dir, x) for x in test]
-		return train, test
+			train_dict[type_dir] = [os.path.join(self.base_path, type_dir, x) for x in train]
+			test_dict[type_dir] = [os.path.join(self.base_path, type_dir, x) for x in test]
+		return train_dict, test_dict
 
 class Fast5Reader:
 
@@ -94,27 +96,21 @@ class Fast5Reader:
 	    if 'Signal' in name:
 	        return name
 
-    def get_sample(self):
-    	with h5py.File(os.path.join(path, file),'r') as contents:
-    		return self._get_sample_from_h5(contents)
+	def get_sample(self):
+		with h5py.File(self.path,'r') as contents:
+			return self._get_sample_from_h5(contents)
 
 	def _get_sample_from_h5(self, h5_file):
-		corrected_events = h5_file['Analyses/RawGenomeCorrected_000/BaseCalled_template/Events']
+		corrected_events = h5_file['Analyses/RawGenomeCorrected_000/BaseCalled_template/Events'][()]
 		raw_signal = h5_file['Raw/Reads']
 		signal_path = raw_signal.visit(self.find_signal)
 		dataset = raw_signal[signal_path]
-		offset = corrected_events.attrs.get('read_start_rel_to_raw')
-		raw_signal = dataset[offset:]
-		index = random.randrange(len(dataset) - self.window_size)
-		signal = dataset[index:(index+self.window_size)]
-		signal_index = corrected_events[0][2]
-		found_first = False
-		seq = []
-		event_index = 0
-		while signal_index < offset + index + self.window_size:
-			if signal_index + corrected_events[event_index][3] > offset + index:
-				found_first = True
-				seq.append(corrected_events[event_index][4])
-			signal_index = signal_index + corrected_events[event_index][3]
-			event_index = event_index + 1
+		offset = h5_file['Analyses/RawGenomeCorrected_000/BaseCalled_template/Events'].attrs.get('read_start_rel_to_raw')
+		print(corrected_events)
+		analyzed_length = corrected_events[-1,2]
+		index = random.randrange(analyzed_length - self.window_size)
+		signal = dataset[(offset+index):(offset+index+self.window_size)]
+		first_event_idx = np.argmax(corrected_events[:,2] >= index)
+		last_event_idx = np.argmax(corrected_events[:,2] >= index + self.window_size)
+		seq = corrected_events[first_event_idx:last_event_idx,4]
 		return signal, seq
