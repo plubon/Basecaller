a9605db209d5667df30a1495153532ea4aaabd95
piotr <plubon1337@gmail.com>
2019-05-07 09:40:19 +0200

    fix

--- basecaller/Data.py
+++ basecaller/Data.py
@@ -19,7 +19,15 @@ class ExampleSequence(keras.utils.Sequence):
 	b'G':2,
 	b'g':2,
 	b'T':3,
-	b't':3
+	b't':3,
+    'A':0,
+	'a':0,
+	'C':1,
+	'c':1,
+	'G':2,
+	'g':2,
+	'T':3,
+	't':3
 	}
 
     def __init__(self, dataset, ids, name='', batch_size=150):
