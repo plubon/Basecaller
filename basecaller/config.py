d979ab5149f4c286391d3f7706a172b3f9a18c8a
piotr.lubon@nex10.pl <piotr.lubon@nex10.pl>
2019-08-19 10:42:26 +0200

    fix

--- basecaller/config.py
+++ basecaller/config.py
@@ -3,7 +3,7 @@ import json
 
 class Config:
 
-    def __init__(self, model_name, epochs, batch_size, optimizer, decoder):
+    def __init__(self, model_name, epochs, batch_size, optimizer, decoder, debug):
         self.model_name = model_name
         self.epochs = epochs
         self.batch_size = batch_size
