eccd00332f5e5265484496ef633e15ef89987ce4
piotr.lubon@nex10.pl <piotr.lubon@nex10.pl>
2019-10-01 22:21:43 +0200

    fix;

--- basecaller/data.py
+++ basecaller/data.py
@@ -295,7 +295,7 @@ class EvalDataExtractor:
         else:
             raise ValueError(f"Format was {filename.split('.')[1]}, but it must be one of {', '.join(['.fast5', '.signal'])}.")
         signal, index = reader.read_for_eval(os.path.join(self.data_dir, filename))
-        signal = np.concatenate(signal)
+        signal = np.stack(signal)
         index = np.array(index)
         filenames = np.repeat(filename, index.shape[0])
         return tf.data.Dataset.from_tensor_slices({
