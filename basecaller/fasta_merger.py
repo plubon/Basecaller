1ddb9448841e428794f1c7c433aaab5365c0a614
piotr.lubon@nex10.pl <piotr.lubon@nex10.pl>
2019-11-12 21:36:35 +0100

    fasta merging

---
+++ basecaller/fasta_merger.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+
+def merge_fasta(input_path, output_path, format):
+    files = os.listdir(input_path)
+    files = [x for x in files if format in x]
+    with open(output_path, 'w') as out:
+        for file in files:
+            with open(os.path.join(input_path, file)) as in_file:
+                seq = in_file.read().strip()
+                out.write(f">{file}\n")
+                out.write(seq)
+                out.write('\n')
+
+
+if __name__ == "__main__":
+    merge_fasta(sys.argv[1], sys.argv[2], sys.argv[3])
+
+
+
+
+
+
+
