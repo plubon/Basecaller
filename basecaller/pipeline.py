b979f391c87c2444260c7b3926326b162249875b
piotr <plubon1337@gmail.com>
2019-11-18 21:25:17 +0100

    fix

--- basecaller/pipeline.py
+++ basecaller/pipeline.py
@@ -29,32 +29,32 @@ def run(config_path,
         time.sleep(60)
         device = get_first_free_device()
     print(f"Using device:{device}", flush=True)
-    with tf.device(f'/device:GPU:{device}'):
-        train_start = datetime.fromtimestamp(time.time()).isoformat()
-        log_time(output_path, 'train_start', train_start)
-        train(config_path, train_dataset_path, val_dataset_path, output_path)
-        train_end = datetime.fromtimestamp(time.time()).isoformat()
-        log_time(output_path, 'train_end', train_end)
-        print(f'Finished training')
-        tf.reset_default_graph()
-        test(output_path, train_dataset_path)
-        tf.reset_default_graph()
-        eval_start = datetime.fromtimestamp(time.time()).isoformat()
-        log_time(output_path, 'eval_start', eval_start)
-        evaluate(output_path, raw_data_path, os.path.join(output_path, 'logits'))
-        eval_end = datetime.fromtimestamp(time.time()).isoformat()
-        log_time(output_path, 'eval_end', eval_end)
-        print('Finished calculating logits')
-        tf.reset_default_graph()
-        assemble_start = datetime.fromtimestamp(time.time()).isoformat()
-        log_time(output_path, 'assemble_start', assemble_start)
-        assemble(os.path.join(output_path, 'logits'),
-                 os.path.join(output_path, 'fasta'),
-                 'beam_search',
-                 'simple')
-        assemble_end = datetime.fromtimestamp(time.time()).isoformat()
-        log_time(output_path, 'assemble_end', assemble_end)
-        print('Finished assembly')
+    os.environ["CUDA_VISIBLE_DEVICES"] = device
+    train_start = datetime.fromtimestamp(time.time()).isoformat()
+    log_time(output_path, 'train_start', train_start)
+    train(config_path, train_dataset_path, val_dataset_path, output_path)
+    train_end = datetime.fromtimestamp(time.time()).isoformat()
+    log_time(output_path, 'train_end', train_end)
+    print(f'Finished training')
+    tf.reset_default_graph()
+    test(output_path, train_dataset_path)
+    tf.reset_default_graph()
+    eval_start = datetime.fromtimestamp(time.time()).isoformat()
+    log_time(output_path, 'eval_start', eval_start)
+    evaluate(output_path, raw_data_path, os.path.join(output_path, 'logits'))
+    eval_end = datetime.fromtimestamp(time.time()).isoformat()
+    log_time(output_path, 'eval_end', eval_end)
+    print('Finished calculating logits')
+    tf.reset_default_graph()
+    assemble_start = datetime.fromtimestamp(time.time()).isoformat()
+    log_time(output_path, 'assemble_start', assemble_start)
+    assemble(os.path.join(output_path, 'logits'),
+             os.path.join(output_path, 'fasta'),
+             'beam_search',
+             'simple')
+    assemble_end = datetime.fromtimestamp(time.time()).isoformat()
+    log_time(output_path, 'assemble_end', assemble_end)
+    print('Finished assembly')
 
 
 if __name__ == "__main__":
