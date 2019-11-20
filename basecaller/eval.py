4cd73987b6ebf0c5ac633a312df2ec192db7c605
piotr <plubon1337@gmail.com>
2019-11-20 10:40:11 +0100

    eval fix

--- basecaller/eval.py
+++ basecaller/eval.py
@@ -11,7 +11,7 @@ from utils import int_label_to_string
 batch_size = 150
 
 
-def evaluate(model_dir, data_dir, out_dir, file_list=None):
+def evaluate(model_dir, data_dir, out_dir, file_list=None, calculate_distance=False):
     data_extractor = EvalDataExtractor(data_dir, file_list)
     logits_input = tf.placeholder(tf.float32, shape=(None, 300, 5), name='decode_logits_input')
     len_input = tf.placeholder(tf.int32, shape=(None,), name='decode_lengths_input')
@@ -40,23 +40,25 @@ def evaluate(model_dir, data_dir, out_dir, file_list=None):
                 logits = sess.run('dense/BiasAdd:0', feed_dict=feed_dict)
                 logits_list.append(logits)
                 decoded_out = sess.run(decoded, feed_dict={logits_input: logits, len_input: lengths[batch_index:batch_index + batch_size]})
-                for indx, bpread in enumerate(decoded_out):
-                    if np.any(bpread == -1):
-                        bpread = bpread[:np.argmax(bpread == -1)]
-                    if len(bpread) == 0:
-                        continue
-                    string_result = int_label_to_string(bpread)
-                    joined_target = ''.join(label[indx])
-                    dist = distance(string_result, joined_target) / len(joined_target)
-                    distances.append(dist)
+                if calculate_distance:
+                    for indx, bpread in enumerate(decoded_out):
+                        if np.any(bpread == -1):
+                            bpread = bpread[:np.argmax(bpread == -1)]
+                        if len(bpread) == 0:
+                            continue
+                        string_result = int_label_to_string(bpread)
+                        joined_target = ''.join(label[indx])
+                        dist = distance(string_result, joined_target) / len(joined_target)
+                        distances.append(dist)
                 batch_index = batch_index + batch_size
             file_logits = np.concatenate(logits_list, axis=0)
             save_file_results(out_dir, file_logits, filename, indices)
             file_results[filename] = np.mean(distances)
             print(f"Processed file :{filename} Distance: {file_results[filename]}")
-    with open(os.path.join(out_dir, 'file_results.csv'),'w') as results_file:
-        for key, value in file_results.items():
-            results_file.write(f"{key},{value}\n")
+    if calculate_distance:
+        with open(os.path.join(out_dir, 'file_results.csv'),'w') as results_file:
+            for key, value in file_results.items():
+                results_file.write(f"{key},{value}\n")
 
 
 def save_file_results(out_dir, logits, filename, indices):
@@ -70,4 +72,4 @@ if __name__ == "__main__":
     with open(sys.argv[4]) as file_list:
         data = json.loads(file_list.read())
         lines = data['files']
-    evaluate(sys.argv[1], sys.argv[2], sys.argv[3], lines)
+    evaluate(sys.argv[1], sys.argv[2], sys.argv[3], lines, calculate_distance=True)
