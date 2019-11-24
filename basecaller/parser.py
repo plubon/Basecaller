87a5a73bfbe306e7de016fe624ed457dfccd6a9c
piotr <plubon1337@gmail.com>
2019-11-24 21:59:09 +0100

    dirtier dataset

---
+++ basecaller/parser.py
@@ -0,0 +1,118 @@
+from utils import string_label_to_int, write_dict_to_file
+from sklearn.model_selection import train_test_split
+from entities import TrainingExample
+from reader import ChironFileReader, H5FileReader, DirtyChironFileReader
+import os
+import tensorflow as tf
+
+
+class BaseFileParser:
+
+    def get_id(self):
+        id = self.id_counter
+        self.id_counter += 1
+        return str(id)
+
+    def __init__(self, input_path, output_path, seg_length=300, skip=10, format='h5', test_size=0.2, val_size=0.2):
+        self.input_path = input_path
+        self.output_path = output_path
+        self.files = os.listdir(self.input_path)
+        self.reader = self.get_reader(format)
+        self.files = self.reader.filter_files(self.files)
+        self.files = [x.split('.')[0] for x in self.files]
+        self.files = list(set(self.files))
+        self.skip = skip
+        self.segment_length = seg_length
+        self.id_counter = 1
+        self.test_size = test_size
+        self.val_size = val_size
+
+    def get_tf_example(self, example):
+        signal = tf.train.FloatList(value=example.signal)
+        signal = tf.train.Feature(float_list=signal)
+        label = tf.train.Int64List(value=string_label_to_int(example.sequence))
+        label = tf.train.Feature(int64_list=label)
+        signal_len = tf.train.Int64List(value=[len(example.signal)])
+        signal_len = tf.train.Feature(int64_list=signal_len)
+        label_len = tf.train.Int64List(value=[len(example.sequence)])
+        label_len = tf.train.Feature(int64_list=label_len)
+        feature_dict = {
+            'signal': signal,
+            'label': label,
+            'signal_len': signal_len,
+            'label_len': label_len
+        }
+        features = tf.train.Features(feature=feature_dict)
+        return tf.train.Example(features=features)
+
+    def create_split_record(self, files, split_name):
+        count = 0
+        test_filename = os.path.join(self.output_path, f'{split_name}.tfrecords')
+        with tf.python_io.TFRecordWriter(test_filename) as writer:
+            for file in files:
+                examples = self.reader.read(os.path.join(self.input_path, file))
+                count += len(examples)
+                for example in examples:
+                    writer.write(self.get_tf_example(example).SerializeToString())
+        info_filename = os.path.join(self.output_path, split_name)
+        write_dict_to_file(info_filename, {
+            'count': count,
+            'files': files
+        })
+
+    def create_record(self):
+        count = 0
+        test_filename = os.path.join(self.output_path, 'dataset.tfrecords')
+        with tf.python_io.TFRecordWriter(test_filename) as writer:
+            for file in self.files:
+                examples = self.reader.read(os.path.join(self.input_path, file))
+                count += len(examples)
+                for example in examples:
+                    writer.write(self.get_tf_example(example).SerializeToString())
+        info_filename = os.path.join(self.output_path, 'info.json')
+        write_dict_to_file(info_filename, {
+            'count': count,
+            'files': self.files
+        })
+
+    def create(self, split=False):
+        if split:
+            train, rest = train_test_split(self.files, train_size=1 - self.test_size - self.val_size)
+            val, test = train_test_split(rest, train_size=self.val_size / (self.val_size + self.test_size))
+            self.create_split_record(train, 'train')
+            self.create_split_record(test, 'test')
+            self.create_split_record(val, 'val')
+        else:
+            self.create_record()
+
+
+class CleanFileParser(BaseFileParser):
+
+    def __init__(self, input_path, output_path, seg_length=300, skip=10, format='h5', test_size=0.2, val_size=0.2):
+        super().__init__(input_path, output_path, seg_length, skip, format, test_size, val_size)
+
+    known_formats = ['h5', 'chiron']
+
+    def get_reader(self, format):
+        if format == 'h5':
+            return H5FileReader(self)
+        elif format == 'chiron':
+            return ChironFileReader(self)
+        else:
+            raise ValueError(f"Format was {format}, but it must be one of {', '.join(self.known_formats)}.")
+
+
+class DirtyFileParser(BaseFileParser):
+
+    def __init__(self, input_path, output_path, seg_length=300, skip=10, format='h5', test_size=0.2, val_size=0.2):
+        super().__init__(input_path, output_path, seg_length, skip, format, test_size, val_size)
+
+    known_formats = ['h5', 'chiron']
+
+    def get_reader(self, format):
+        if format == 'h5':
+            return H5FileReader(self)
+        elif format == 'chiron':
+            return DirtyChironFileReader(self)
+        else:
+            raise ValueError(f"Format was {format}, but it must be one of {', '.join(self.known_formats)}.")
--- basecaller/reader.py
+++ basecaller/reader.py
@@ -2,6 +2,7 @@ import numpy as np
 import h5py
 from utils import alphabet_dict
 from entities import TrainingExample
+import random
 
 class H5FileReader:
 
@@ -155,6 +156,109 @@ class ChironFileReader:
                             if i - current_start > 4:
                                 signal = dataset[event_position[current_start]: event_position[
                                                                                     current_start] + self.parser.segment_length]
+                                normalized_signal = (signal - np.mean(np.unique(dataset))) / np.std(np.unique(dataset))
+                                breaks = event_position[current_start + 1:i - 1] - event_position[current_start]
+                                current_seq = sequence[current_start:i - 1]
+                                example = TrainingExample(self.parser.get_id(), path, current_start, normalized_signal,
+                                                          signal, current_seq, breaks)
+                                examples.append(example)
+                            current_len = 0
+                            current_start = i + 1
+                            i += 1
+                return examples
+
+    def read_for_eval(self, path):
+        with open(path + '.label', 'r') as label_file:
+            with open(path + '.signal', 'r') as signal_file:
+                dataset = signal_file.readlines()
+                dataset = dataset[0].strip()
+                dataset = dataset.split(' ')
+                dataset = [int(x) for x in dataset]
+                corrected_events_array = label_file.readlines()
+                corrected_events_array = [x.strip() for x in corrected_events_array]
+                corrected_events_array = [x.split(' ') for x in corrected_events_array]
+                event_position = np.array([int(x[0]) for x in corrected_events_array])
+                event_length = np.array([int(x[1]) - int(x[0]) for x in corrected_events_array])
+                sequence = np.array([x[2] for x in corrected_events_array])
+                segments = []
+                labels = []
+                indices = []
+                signal_index = event_position[0]
+                label_index = 0
+                end = event_position[-1] + event_length[-1]
+                while signal_index + 300 < end and signal_index + 300 < len(dataset):
+                    signal = dataset[signal_index:signal_index + 300]
+                    normalized_signal = (signal - np.mean(np.unique(dataset))) / np.std(np.unique(dataset))
+                    label_end = label_index + 1
+                    while label_end < len(event_position) and event_position[label_end] <= signal_index + 300:
+                        label_end = label_end + 1
+                    label = sequence[label_index:label_end]
+                    for idx, char in enumerate(label):
+                        if char not in alphabet_dict.keys():
+                            label[idx] = b'A'
+                    segments.append(normalized_signal)
+                    labels.append(label)
+                    indices.append(signal_index)
+                    signal_index = signal_index + 30
+                    if signal_index + 300 >= end:
+                        break
+                    while label_index + 1 < len(event_position) and event_position[label_index + 1] < signal_index:
+                        label_index = label_index + 1
+                return segments, labels, indices
+
+
+class DirtyChironFileReader:
+
+    def __init__(self, parser=None):
+        self.parser = parser
+
+    def filter_files(self, files):
+        return [x for x in files if x.endswith('signal') or x.endswith('label')]
+
+    def read_entire_sequence(self, path):
+        with open(path, 'r') as label_file:
+            corrected_events_array = label_file.readlines()
+            corrected_events_array = [x.strip() for x in corrected_events_array]
+            corrected_events_array = [x.split(' ') for x in corrected_events_array]
+            sequence = np.array([x[2] for x in corrected_events_array])
+            for idx, char in enumerate(sequence):
+                if char not in alphabet_dict.keys():
+                    sequence[idx] = 'A'
+            return ''.join(sequence)
+
+    def read(self, path):
+        skip = 4
+        with open(path + '.label', 'r') as label_file:
+            with open(path + '.signal', 'r') as signal_file:
+                dataset = signal_file.readlines()
+                dataset = dataset[0].strip()
+                dataset = dataset.split(' ')
+                dataset = [int(x) for x in dataset]
+                corrected_events_array = label_file.readlines()
+                corrected_events_array = [x.strip() for x in corrected_events_array]
+                corrected_events_array = [x.split(' ') for x in corrected_events_array]
+                event_position = np.array([int(x[0]) for x in corrected_events_array])
+                event_length = np.array([int(x[1]) - int(x[0]) for x in corrected_events_array])
+                sequence = np.array([x[2] for x in corrected_events_array])
+                i = skip
+                current_start = i
+                current_len = 0
+                examples = []
+                while i < len(event_position) - skip:
+                    if sequence[i] not in alphabet_dict.keys():
+                        current_len = 0
+                        current_start = i + 1
+                        i += 1
+                    else:
+                        if current_len + event_length[i] <= self.parser.segment_length:
+                            current_len += event_length[i]
+                            i += 1
+                        else:
+                            if i - current_start > 2:
+                                max_back = min(event_length[current_start-1], current_len + event_length[i] - self.parser.segment_length)
+                                back = random.randint(0, max_back)
+                                signal = dataset[event_position[current_start]-back: event_position[
+                                                                                    current_start] + self.parser.segment_length - back]
                                 normalized_signal = (signal - np.mean(np.unique(dataset))) / np.std(np.unique(dataset))
                                 breaks = event_position[current_start + 1:i - 1] - event_position[current_start]
                                 current_seq = sequence[current_start:i - 1]
