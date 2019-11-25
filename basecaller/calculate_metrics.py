33b64dc29538411a11ac8b527f15c5e75cc2e1c7
piotr <plubon1337@gmail.com>
2019-11-25 15:42:32 +0100

    fix;

--- basecaller/calculate_metrics.py
+++ basecaller/calculate_metrics.py
@@ -1,7 +1,7 @@
 from fasta_merger import merge_fasta
 import json
 import os
-
+import sys
 
 def calculate(fasta_path, out_path):
     with open('metrics_settings.json', 'r') as json_file:
@@ -11,3 +11,7 @@ def calculate(fasta_path, out_path):
     os.system(f"{json_data['minimap']} -a {json_data['reference']} {os.path.join(out_path, 'merged.fasta')} > {os.path.join(out_path, 'merged.sam')}")
     os.system(f"{json_data['samtools']} view -S -b {os.path.join(out_path, 'merged.sam')} > {os.path.join(out_path, 'merged.bam')}")
     os.system(f"{json_data['hts']} --bamFile={os.path.join(out_path, 'merged.bam')} --reference={json_data['reference']} > {os.path.join(out_path, 'metrics.outs')}")
+
+
+if __name__ == "__main__":
+    calculate(sys.argv[1], sys.argv[2])
