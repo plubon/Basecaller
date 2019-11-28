218d0704354661048017ef978da8a94f3a80db31
piotr <plubon1337@gmail.com>
2019-11-28 18:56:29 +0100

    fix

--- basecaller/calculate_metrics.py
+++ basecaller/calculate_metrics.py
@@ -11,7 +11,7 @@ def calculate(fasta_path, out_path):
     os.system(f"{json_data['minimap']} -a {json_data['ecoli_reference']} {os.path.join(out_path, 'ecoli_merged.fasta')} > {os.path.join(out_path, 'ecoli_merged.sam')}")
     os.system(f"{json_data['samtools']} view -S -b {os.path.join(out_path, 'ecoli_merged.sam')} > {os.path.join(out_path, 'ecoli_merged.bam')}")
     os.system(f"{json_data['hts']} --bamFile={os.path.join(out_path, 'ecoli_merged.bam')} --reference={json_data['ecoli_reference']} > {os.path.join(out_path, 'ecoli_metrics.outs')}")
-    merge_fasta(fasta_path, os.path.join(out_path, 'ecoli_merged.fasta'), format='Lambda')
+    merge_fasta(fasta_path, os.path.join(out_path, 'lambda_merged.fasta'), format='Lambda')
     os.system(f"{json_data['minimap']} -a {json_data['lambda_reference']} {os.path.join(out_path, 'lambda_merged.fasta')} > {os.path.join(out_path, 'lambda_merged.sam')}")
     os.system(f"{json_data['samtools']} view -S -b {os.path.join(out_path, 'lambda_merged.sam')} > {os.path.join(out_path, 'lambda_merged.bam')}")
     os.system(f"{json_data['hts']} --bamFile={os.path.join(out_path, 'lambda_merged.bam')} --reference={json_data['lambda_reference']} > {os.path.join(out_path, 'lambda_metrics.outs')}")
